a0e2d8d94d94b354e6565fe3b78ecd5dc36d2a20
James Robinson <jpwrobinson@gmail.com>
2022-11-23 12:55:50 +0000

    details for next week

--- scripts/02_emergence.R
+++ scripts/02_emergence.R
@@ -1,11 +1,20 @@
-
-
-## Read in Emergence
+## Read in Emergence dataset
 source('scripts/01_senescence.R')
 
-## Old = baseline of stem counts
-## New = change in stem counts taken at 3 days
-emerg<-read_excel('data/BOG_Emergence.xls')
+# Block = same as BOG_sen_details, but lower case letter
+# Type = same as BOG_sen_details, but lower case letters
+
+## Old = baseline of stem counts (with date format MM/YY/DD)
+## New = change in stem counts taken at 3 days (with date format MM/YY/DD)
+emerg<-read_excel('data/BOG_Emergence.xls', na = "NA")
+
+## Things to do:
+# trying to replicate the format of BOG_sen (created in scripts/01_scenescence.R)
+# clean the ID columns (block, type)
+# create columns for date and count
+# this will probably need pivot_longer and verbs in the stringr package
+head(BOG_sen)
+head(emerg)
 
 ## Tim's slot
 library(tidyverse)
