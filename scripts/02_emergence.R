d3c1ba264d09e7db754b53b9a2a7b683300d4ae4
James Robinson <jpwrobinson@gmail.com>
2022-11-23 13:00:13 +0000

    cleaned end of session

--- scripts/02_emergence.R
+++ scripts/02_emergence.R
@@ -7,7 +7,7 @@ source('scripts/01_senescence.R')
 ## Old = baseline of stem counts (with date format MM/YY/DD)
 ## New = change in stem counts taken at 3 days (with date format MM/YY/DD)
 emerg<-read_excel('data/BOG_Emergence.xls', na = "NA")
-
+  
 ## Things to do:
 # trying to replicate the format of BOG_sen (created in scripts/01_scenescence.R)
 # clean the ID columns (block, type)
@@ -16,19 +16,7 @@ emerg<-read_excel('data/BOG_Emergence.xls', na = "NA")
 head(BOG_sen)
 head(emerg)
 
-## Tim's slot
-library(tidyverse)
-library(readxl)
-BOG_emerge <- read_excel("data/BOG_Emergence.xls",na = "NA")
-head(BOG_emerge)
-view(BOG_emerge)
-
-### Leonardo's slot
-BOG_emerg <- read_excel("data/BOG_Emergence.xls",na = "NA")
-
-# Rucha
-
-BOG_emerge <- read_excel("data/BOG_Emergence.xls",na = "NA") %>%
+emerg_long<-emerg %>% 
   group_by(block,type) %>% 
   pivot_longer(cols = (new051508:new061512),
                names_to = "interval", values_to = "emerge") 
