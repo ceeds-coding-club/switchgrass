cda6b7d996e6d66cac55085dfb790adc05afc0fc
James Robinson <jpwrobinson@gmail.com>
2022-11-23 12:44:46 +0000

    james data read

--- scripts/02_emergence.R
+++ scripts/02_emergence.R
@@ -1,4 +1,10 @@
 ## Read in Emergence
+source('scripts/01_senescence.R')
+
+## Old = baseline of stem counts
+## New = change in stem counts taken at 3 days
+emerg<-read_excel('data/BOG_Emergence.xls')
+
 
 ## Tim's slot
 
