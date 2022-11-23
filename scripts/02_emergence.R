52237ff2c006517a090afc50758d61d9a7b7ba97
Samuel Taylor <s.taylor19@lancaster.ac.uk>
2022-11-23 12:55:59 +0000

    sam read emerg change

--- scripts/02_emergence.R
+++ scripts/02_emergence.R
@@ -9,5 +9,15 @@
 
 ## Sam
 
+#read data
+BOG_emr <- read_excel("data/BOG_Emergence.xls", na = "NA") %>%
+  pivot_longer(cols = -(pop:popblock),names_to = "date", values_to = "senesc") %>%
+  mutate(across(pop:popblock, as_factor),
+         date = as.Date(as.numeric(date), origin = "1899-12-30"),
+         day_of_year = julian(date, origin = as.Date("2014-01-01"))
+  )
+
+head(BOG_sen)
+summary(BOG_sen)
 
 # Rucha
