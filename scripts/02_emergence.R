8ffddb14b1c7286b81847732a50377ca5a814d7c
Tim Lamont <tim.lamont@lancaster.ac.uk>
2022-11-23 12:40:03 +0000

    Tim's bit

--- scripts/02_emergence.R
+++ scripts/02_emergence.R
@@ -1,7 +1,11 @@
 ## Read in Emergence
 
 ## Tim's slot
-
+library(tidyverse)
+library(readxl)
+BOG_emerge <- read_excel("data/BOG_Emergence.xls",na = "NA")
+head(BOG_emerge)
+view(BOG_emerge)
 
 
 ### Leonardo's slot
